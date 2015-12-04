6e02c7700845d2294cd57bfe645d1bd34ba0a918
Ellyn Howard <ellynhoward@Ellyn-Howards-MacBook-Pro.local>
2015-12-04 10:10:05 -0800

    in HTML added JavaScript tags one-five in headers. In js, removed alerts and replaced with one.textContent so responses appear in browser, not in pop ups anymore.

--- game.js
+++ game.js
@@ -6,49 +6,59 @@ console.log('the user variable ' + user);
 alert('Nice to virtually meet you, ' +  user + ', hope you are having a great day!')
 
 //beginning of guessing game portion
-
+var one = document.getElementById('one');
 var i = 0;
 function quesOne(){
 
   var like = prompt('Do dogs bark?');
     if(like == 'yes' || like == 'YES' || like == 'Yes' || like == 'Y' || like == 'y')
     {
-      alert('You are right! Dogs do bark!');
+      // alert('You are right! Dogs do bark!');
       i++;
       console.log('var i = ' + i);
+      one.textContent = ('You are right! Dogs do bark!');
+      // console
     }
     else {
-      alert('You are incorrect, dogs do bark.');
+      // alert('You are incorrect, dogs do bark.');
+      one.textContent = ('You are incorrect, dogs do bark.')
     }
 }
 quesOne();
 
+var one = document.getElementById('two');
 function quesTwo() {
 
     var have = prompt('Does Starbucks sell coffee?');
     if(have == 'yes' || have == 'YES' || have == 'Y' || have == 'y' || have == 'Yes')
     {
-      alert('You are right! Starbucks does sell coffee!');
+      // alert('You are right! Starbucks does sell coffee!');
       i++;
       console.log('var i = ' + i);
+      one.textContent = 'You are right! Starbucks does sell coffee!'
     }
     else{
-      alert('Hmm, have you ever been to Starbucks? They sell a lot of coffee!');
+      // alert('Hmm, have you ever been to Starbucks? They sell a lot of coffee!');
+      one.textContent = ('Hmm, have you ever been to Starbucks? They sell a lot of coffee!')
+
     }
   }
 quesTwo();
 
+var one = document.getElementById('three');
 function quesThree(){
 
     var want = prompt('Is Seattle in Washington?')
     if(want == 'yes' || want == 'Yes' || want == 'YES' || want == 'Y' || want == 'y')
     {
-      alert('You are right! Seattle is in Washington');
+      // alert('You are right! Seattle is in Washington');
       i++;
       console.log('var i = ' + i);
+      one.textContent = ('You are right! Seattle is in Washington')
     }
     else{
-      alert('You are incorrect Seattle is in Washington.');
+      // alert('You are incorrect Seattle is in Washington.');
+      one.textContent = ('You are incorrect, Seattle is in Washington')
     }
   }
 quesThree();
@@ -65,6 +75,7 @@ alert('I am thinking of a number...');
 console.log('var user = ' + guess);
 var guess = prompt('Can you guess what it is?');
 
+var one = document.getElementById('four');
 function quesFour(){
 
     var i = 3;
@@ -73,20 +84,20 @@ function quesFour(){
         while(guess > i)
         {
           msg = ' you guessed ' + guess + ', that is too high ';
-          alert(user + msg);
+          one.textContent = (user + msg);
           guess = prompt(' guess again! ');
         }
 
         while(guess < i)
         {
           msg = ' you guessed ' + guess + ', that is too low ';
-          alert(user + msg);
+          one.textContent = (user + msg);
           guess = prompt(' guess again! ');
         }
 
         if(guess == i)
         {
-          var answer = alert('you are right! lets move on!');
+          var answer = one.textContent = ('you are right, my number is 3! lets move on!');
         }
     }
   quesFour();
@@ -96,26 +107,30 @@ function quesFour(){
 var transportation = prompt('What is the best way of transportation for Seattlites?');
 var arr = ['car', 'bus', 'bike', 'walk'];
 
+var one = document.getElementById('five');
 function quesFive(){
 
       for (var i = 0; i < arr.length; i++)
       {
 
         if (transportation == 'car' || transportation == 'cars' || transportation == 'driving'){
-          alert('You said ' + transportation + ' is the best way to get around Seattle!');
+          one.textContent = ('You said ' + transportation + ' is the best way to get around Seattle!');
           break;
         }
         if (transportation == 'bus' || transportation == 'busses' || transportation == 'bussing'){
-          alert('You said ' + transportation + ' is the best way to get around Seattle!');
+          one.textContent = ('You said ' + transportation + ' is the best way to get around Seattle!');
           break;
         }
         if (transportation == 'bike' || transportation == 'bikes' || transportation == 'biking'){
-          alert('You said ' + transportation + ' is the best way to get around Seattle!');
+          one.textContent = ('You said ' + transportation + ' is the best way to get around Seattle!');
           break;
         }
         if (transportation == 'walk' || transportation == 'foot' || transportation == 'walking'){
-          alert('You said ' + transportation + ' is the best way to get around Seattle!');
+          one.textContent = ('You said ' + transportation + ' is the best way to get around Seattle!');
           break;
+        }
+        else {
+          one.textContent = ('You said ' + transportation + '. That is an interesting choice of transportation')
         }
       }
     }

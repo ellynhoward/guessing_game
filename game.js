8eb623a422524d1e657a6e1ab3fd1ec8f96c4751
Ellyn Howard <ellynhoward@Ellyn-Howards-MacBook-Pro.local>
2015-12-04 09:02:36 -0800

    added functions around all questions

--- game.js
+++ game.js
@@ -8,7 +8,9 @@ alert('Nice to virtually meet you, ' +  user + ', hope you are having a great da
 //beginning of guessing game portion
 
 var i = 0;
-var like = prompt('Do dogs bark?');
+function quesOne(){
+
+  var like = prompt('Do dogs bark?');
     if(like == 'yes' || like == 'YES' || like == 'Yes' || like == 'Y' || like == 'y')
     {
       alert('You are right! Dogs do bark!');
@@ -18,9 +20,12 @@ var like = prompt('Do dogs bark?');
     else {
       alert('You are incorrect, dogs do bark.');
     }
+}
+quesOne();
 
+function quesTwo() {
 
-var have = prompt('Does Starbucks sell coffee?');
+    var have = prompt('Does Starbucks sell coffee?');
     if(have == 'yes' || have == 'YES' || have == 'Y' || have == 'y' || have == 'Yes')
     {
       alert('You are right! Starbucks does sell coffee!');
@@ -30,8 +35,12 @@ var have = prompt('Does Starbucks sell coffee?');
     else{
       alert('Hmm, have you ever been to Starbucks? They sell a lot of coffee!');
     }
+  }
+quesTwo();
 
-var want = prompt('Is Seattle in Washington?')
+function quesThree(){
+
+    var want = prompt('Is Seattle in Washington?')
     if(want == 'yes' || want == 'Yes' || want == 'YES' || want == 'Y' || want == 'y')
     {
       alert('You are right! Seattle is in Washington');
@@ -41,8 +50,73 @@ var want = prompt('Is Seattle in Washington?')
     else{
       alert('You are incorrect Seattle is in Washington.');
     }
+  }
+quesThree();
 
 
 var total = i;
 console.log('total = ' + i)
 alert('You have answered ' + i + ' out of 3 correct!')
+
+
+// while loop portion
+
+alert('I am thinking of a number...');
+console.log('var user = ' + guess);
+var guess = prompt('Can you guess what it is?');
+
+function quesFour(){
+
+    var i = 3;
+    var msg = ' ';
+
+        while(guess > i)
+        {
+          msg = ' you guessed ' + guess + ', that is too high ';
+          alert(user + msg);
+          guess = prompt(' guess again! ');
+        }
+
+        while(guess < i)
+        {
+          msg = ' you guessed ' + guess + ', that is too low ';
+          alert(user + msg);
+          guess = prompt(' guess again! ');
+        }
+
+        if(guess == i)
+        {
+          var answer = alert('you are right! lets move on!');
+        }
+    }
+  quesFour();
+
+// for loop portion
+
+var transportation = prompt('What is the best way of transportation for Seattlites?');
+var arr = ['car', 'bus', 'bike', 'walk'];
+
+function quesFive(){
+
+      for (var i = 0; i < arr.length; i++)
+      {
+
+        if (transportation == 'car' || transportation == 'cars' || transportation == 'driving'){
+          alert('You said ' + transportation + ' is the best way to get around Seattle!');
+          break;
+        }
+        if (transportation == 'bus' || transportation == 'busses' || transportation == 'bussing'){
+          alert('You said ' + transportation + ' is the best way to get around Seattle!');
+          break;
+        }
+        if (transportation == 'bike' || transportation == 'bikes' || transportation == 'biking'){
+          alert('You said ' + transportation + ' is the best way to get around Seattle!');
+          break;
+        }
+        if (transportation == 'walk' || transportation == 'foot' || transportation == 'walking'){
+          alert('You said ' + transportation + ' is the best way to get around Seattle!');
+          break;
+        }
+      }
+    }
+  quesFive();
